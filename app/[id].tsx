5f19be4a9b4a476b3b65cc130cc27f6f9df8d584
partharoyy <69928055+partharoyy@users.noreply.github.com>
2024-10-23 17:20:17 +0530

    Footer

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 
 import events from '~/assets/events.json';
 
@@ -29,6 +29,14 @@ const Event = () => {
       <Text className="text-lg" numberOfLines={2}>
         {event.description}
       </Text>
+
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
+        <Text className="text-xl font-semibold">Free</Text>
+
+        <Pressable className="rounded-md bg-red-500 p-5 px-8">
+          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
